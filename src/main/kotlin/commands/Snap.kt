ef32b190087d2abfe24f412bf6e8075064458609
Eyram Hlorgbe <eyram.hlorgbe@hubtel.com>
2024-06-27 10:13:01 +0000

    Store commit in db

--- src/main/kotlin/commands/Snap.kt
+++ src/main/kotlin/commands/Snap.kt
@@ -1,12 +1,16 @@
 package com.sunniercherries.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
 import com.sunniercherries.models.*
 
 
 class Snap : CliktCommand(
     help = "Takes a shot of your project's current state."
 ) {
+
+    private val message by option(names = arrayOf("--message", "-m"), help = "yelp!")
+
     override fun run() {
         val entries = WorkSpace.getFilePaths()?.map { path ->
 
@@ -17,16 +21,18 @@ class Snap : CliktCommand(
             Entry(path.name, blob.hash)
         }
 
-        val tree = entries?.let {
-            Tree(it)
-        }
+        if (entries == null) return
 
-        val didSucceed = tree?.run {
-            Database.store(this)
-        }
+        val tree = Tree(entries)
+        Database.store(tree)
 
-        if (didSucceed == true) {
-            echo("Successfully snapped a file")
+        val author = Author(name = "Eyram Hlorgbe", email = "eyram.hlorgbe@hubtel.com")
+
+        val commit = Commit(tree, author, message ?: "")
+        Database.store(commit)
+
+        WorkSpace.run {
+            writeFile(HEAD_FILE, commit.hash)
         }
     }
 }
